642d602e58cce8808a555db9bad4f1a2f6d2a723
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-01-25 19:34:35 +0600

    package

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,4 +1,4 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
 import authSlice from './features/auth/authSlice';
 import postSlice from './features/post/postSlice'
 import commentSlice from './features/comment/commentSlice'
@@ -9,4 +9,9 @@ export const store = configureStore({
 		post: postSlice,
 		comment: commentSlice,
 	},
+	// middleware:getDefaultMiddleware=>getDefaultMiddleware().concat(authSlice.middleware)
+	// .concat(postSlice.middleware)
+	// .concat(commentSlice.middleware),
+	// devTools:false
+
 })
